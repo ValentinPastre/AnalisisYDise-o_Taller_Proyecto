e590c924143888cc11ba4c03559e80be7876f043
FlanYamkee <franandreani@gmail.com>
2025-05-29 13:22:39 -0300

    Minima modificacion al Readme y al schema

--- kickStart-app/db/schema.rb
+++ kickStart-app/db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_16_135225) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_29_142336) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 end
