c68bcf2362fba9fc04cc397fdc4953015d53a627
fabri995Parejo <fabricioParejo995@gmail.com>
2025-05-29 23:52:53 -0300

    comentario en addTypeToService

--- kickStart-app/db/migrate/20250530022803_add_type_to_services.rb
+++ kickStart-app/db/migrate/20250530022803_add_type_to_services.rb
@@ -1,5 +1,6 @@
 class AddTypeToServices < ActiveRecord::Migration[8.0]
   def change
-    add_column :services, :type, :string
+    add_column :service, :type, :string
   end
 end
+#guarda automáticamente el nombre de la subclase (ej: Agua) en la columna type cuando crees una instancia de esa clase.
