2a4545c9ef9d5292d698653ffa70b450641bd31b
ValentinPastre <valentinpastre.uni@gmail.com>
2025-06-08 17:45:09 -0300

    agregada relacion con 'savings'

--- kickStart-app/models/account.rb
+++ kickStart-app/models/account.rb
@@ -2,5 +2,5 @@ class Account < ActiveRecord::Base
   belongs_to :user
   has_one :security_questions
   has_many :source_transactions, class_name: 'Transaction', foreign_key: :source_account_id
-  has_many :savings
+  has_many :savings, class_name: 'Saving'
 end
