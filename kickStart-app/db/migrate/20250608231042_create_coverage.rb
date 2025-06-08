7755a92e8f09b8d9a77aac20a815bf62ada1196c
ValentinPastre <valentinpastre.uni@gmail.com>
2025-06-08 20:17:24 -0300

    Creado el modelo coverage junto a su migracion

---
+++ kickStart-app/db/migrate/20250608231042_create_coverage.rb
@@ -0,0 +1,9 @@
+class CreateCoverage < ActiveRecord::Migration[8.0]
+  def change
+    create_table :coverages do |t|
+      t.references :obras_sociales, null: false, foreign_key: true
+      t.references :services, null: false, foreign_key: true
+      t.integer :coverage_percentage
+    end
+  end
+end
