8edd0ccfbeb950b8f731f4bbf138ada4010651f1
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-15 21:48:00 -0300

    modifico comentario

--- kickStart-app/db/migrate/20250616004156_add_type_to_services.rb
+++ kickStart-app/db/migrate/20250616004156_add_type_to_services.rb
@@ -1,6 +1,7 @@
 class AddTypeToServices < ActiveRecord::Migration[8.0]
   def change
     add_column :services, :type, :string
-    #añadir la columna type a services sirve para poder identificar que subclase de servicio es
+    #añadir la columna type a services sirve para poder identificar que subclase de servicio es.
+    #el servicio puede ser de agua, luz, gas, o transportationCard
   end
 end
