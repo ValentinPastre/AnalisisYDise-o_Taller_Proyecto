7d473de20ea01c6e64cb87aee4a8c09c3ea7dd25
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-15 21:47:01 -0300

    Agrego comentario a migracion

--- kickStart-app/db/migrate/20250616004156_add_type_to_services.rb
+++ kickStart-app/db/migrate/20250616004156_add_type_to_services.rb
@@ -1,5 +1,6 @@
 class AddTypeToServices < ActiveRecord::Migration[8.0]
   def change
     add_column :services, :type, :string
+    #añadir la columna type a services sirve para poder identificar que subclase de servicio es
   end
 end
