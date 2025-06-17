7832f6119def9a2a65215854d8cee1d7dc386d84
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-17 19:42:38 -0300

    Agrego comentarios a migracion addTargetAccountToServices

--- kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
+++ kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
@@ -1,4 +1,6 @@
 class AddTargetAccountToServices < ActiveRecord::Migration[8.0]
   def change
-  end
+    #Agregamos un campo, target_account_id
+    #nos sirve para saber cual es la cuenta de destino del pago
+    
 end
