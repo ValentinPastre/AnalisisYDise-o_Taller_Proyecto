e448b8e2fc735aac698d897cde4a2545e3f2ebe3
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-17 19:45:44 -0300

    modifico migracion addTargetAccountToServices para que añada columna a services

--- kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
+++ kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
@@ -1,6 +1,9 @@
 class AddTargetAccountToServices < ActiveRecord::Migration[8.0]
   def change
-    #Agregamos un campo, target_account_id
+    #Agregamos un campo, target_account
+
+    add_reference :services, :target_account, foreing_key: {to_table: :accounts}
+
     #nos sirve para saber cual es la cuenta de destino del pago
-    
+
 end
