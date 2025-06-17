5bf6726fd8b7e517021cf82ceed176cde171ce34
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-17 19:55:15 -0300

    Fix migracion

--- kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
+++ kickStart-app/db/migrate/20250617224012_add_target_account_to_services.rb
@@ -2,7 +2,7 @@ class AddTargetAccountToServices < ActiveRecord::Migration[8.0]
   def change
     #Agregamos un campo, target_account
 
-    add_reference :services, :target_account, foreing_key: {to_table: :accounts}
+    add_reference :services, :target_account, foreign_key: {to_table: :accounts}
     #foreing_key, crea una asociacion hacia la tabla accounts, no a una tabla target_accounts
     #nos sirve para saber cual es la cuenta de destino del pago
   end
