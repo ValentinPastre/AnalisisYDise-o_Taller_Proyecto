6756d187dc4636985bdbb688cdd50d089f576ebb
fabri995Parejo <fabricioparejo995@gmail.com>
2025-06-18 02:23:03 -0300

    Fix/Modificacion de la migracion

--- kickStart-app/models/transportation_card.rb
+++ kickStart-app/models/transportation_card.rb
@@ -1,4 +1,4 @@
-class transportationCard < Service
+class TransportationCard < Service
     #number y balance va a venir directamente de la base de datos
     #estos campos van a estar en services
     #aunque sean propios de transportationcard van a estar en services
